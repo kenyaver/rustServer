5de6074baa2937081afea0679b55ca8341514bd9
artem garipov <garipov@zelax.ru>
2024-07-04 15:10:08 +0300

    first server version with get-response on HTTP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
 fn main() {
-    println!("Hello, world!");
+    let tcp_listener = TcpListener::bind("192.168.10.92:1500").unwrap();
+    for stream in tcp_listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let http_reqest: Vec<_> = buf_reader
+    .lines()
+    .map(|result|result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+    println!("http: {http_reqest:#?}");
+
+    let http_response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(http_response.as_bytes()).unwrap();
 }
