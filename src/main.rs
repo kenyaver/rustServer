8044a8eb50020d6620091c5593d80ed47e33a9bf
artem garipov <garipov@zelax.ru>
2024-07-04 16:13:49 +0300

    multithread

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
-use std::{fs, io::{BufRead, BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{fs,
+        io::{BufRead, BufReader, Write},
+        net::{TcpListener, TcpStream},
+        thread,
+        time::Duration};
 fn main() {
     let tcp_listener = TcpListener::bind("192.168.10.92:1500").unwrap();
+    // let pool = ThreadPool::new();
+
     for stream in tcp_listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(|| handle_connection(stream));
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let http_reqest: Vec<_> = buf_reader
-    .lines()
-    .map(|result|result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect();
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
 
-    println!("http: {http_reqest:#?}");
 
-    let http_response = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("hello.html").unwrap();
-    let content_lenght = content.len();
-    let response = format!("{http_response}\r\nContent-Length: {content_lenght}\r\n\r\n{content}");
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
 }
